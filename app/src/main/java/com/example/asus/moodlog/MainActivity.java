84ccb7eed3ff6bf2710b1a7f5268dc7db1d0c6f6
asus <2034233226@qq.com>
2019-06-21 01:20:05 +0800

    fighting

--- app/src/main/java/com/example/asus/moodlog/MainActivity.java
+++ app/src/main/java/com/example/asus/moodlog/MainActivity.java
@@ -3,42 +3,68 @@ package com.example.asus.moodlog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
+    private Fragment mFragments[];
+    private RadioGroup radioGroup;
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
+    private RadioButton rbtHome,rbtFunc,rbtMyinfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFragments = new Fragment[3];
+        fragmentManager = getSupportFragmentManager();
+        mFragments[0] = fragmentManager.findFragmentById(R.id.fragment_main);
+        mFragments[1] = fragmentManager.findFragmentById(R.id.fragment_func);
+        mFragments[2] = fragmentManager.findFragmentById(R.id.fragment_myinfo);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        fragmentTransaction = fragmentManager.beginTransaction().hide(mFragments[0]).hide(mFragments[1]).hide(mFragments[2]);
+        fragmentTransaction.show(mFragments[0]).commit();
+        radioGroup = (RadioGroup)findViewById(R.id.bottomGroup);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                Log.i("radioGroup", "checkId=" + checkedId);
+                fragmentTransaction = fragmentManager.beginTransaction().hide(mFragments[0]).hide(mFragments[1]).hide(mFragments[2]);
+
+                switch(checkedId){
+                    case R.id.radioHome:
+                        fragmentTransaction.show(mFragments[0]).commit();
+                        break;
+                    case R.id.radioFunc:
+                        fragmentTransaction.show(mFragments[1]).commit();
+                        break;
+                    case R.id.radioMyinfo:
+                        fragmentTransaction.show(mFragments[2]).commit();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+
+
+
     }
+
+
+
 
 }
