470a88e46caef75246232dce92a18d263cd932d7
asus <2034233226@qq.com>
2019-06-22 17:10:08 +0800

    addlog

--- app/src/main/java/com/example/asus/moodlog/DBManager.java
+++ app/src/main/java/com/example/asus/moodlog/DBManager.java
@@ -1,4 +1,89 @@
 package com.example.asus.moodlog;
 
-public class DBManager {
-}
+
+    import java.util.ArrayList;
+    import java.util.List;
+
+    import android.content.ContentValues;
+    import android.content.Context;
+    import android.database.Cursor;
+    import android.database.sqlite.SQLiteDatabase;
+
+    public class DBManager {
+
+        private DBHelper dbHelper;
+        private String TBNAME;
+
+        public DBManager(Context context) {
+            dbHelper = new DBHelper(context);
+            TBNAME = DBHelper.TB_NAME;
+        }
+
+        public void add(LogItem item){
+            SQLiteDatabase db = dbHelper.getWritableDatabase();
+            ContentValues values = new ContentValues();
+            values.put("logs", item.getLogs());
+            values.put("time", item.getTime());
+            db.insert(TBNAME, null, values);
+            db.close();
+        }
+
+
+        public void deleteAll(){
+            SQLiteDatabase db = dbHelper.getWritableDatabase();
+            db.delete(TBNAME,null,null);
+            db.close();
+        }
+
+        public void delete(int id){
+            SQLiteDatabase db = dbHelper.getWritableDatabase();
+            db.delete(TBNAME, "ID=?", new String[]{String.valueOf(id)});
+            db.close();
+        }
+
+        public void update(LogItem item){
+            SQLiteDatabase db = dbHelper.getWritableDatabase();
+            ContentValues values = new ContentValues();
+            values.put("logs", item.getLogs());
+            values.put("time", item.getTime());
+            db.update(TBNAME, values, "ID=?", new String[]{String.valueOf(item.getId())});
+            db.close();
+        }
+
+        public List<LogItem> listAll(){
+            List<LogItem> logList = null;
+            SQLiteDatabase db = dbHelper.getReadableDatabase();
+            Cursor cursor = db.query(TBNAME, null, null, null, null, null, null);
+            if(cursor!=null){
+                logList = new ArrayList<LogItem>();
+                while(cursor.moveToNext()){
+                    LogItem item = new LogItem();
+                    item.setId(cursor.getInt(cursor.getColumnIndex("ID")));
+                    item.setLogs(cursor.getString(cursor.getColumnIndex("LOGS")));
+                    item.setTime(cursor.getString(cursor.getColumnIndex("TIME")));
+
+                    logList.add(item);
+                }
+                cursor.close();
+            }
+            db.close();
+            return logList;
+
+        }
+
+        public LogItem findById(int id){
+            SQLiteDatabase db = dbHelper.getReadableDatabase();
+            Cursor cursor = db.query(TBNAME, null, "ID=?", new String[]{String.valueOf(id)}, null, null, null);
+            LogItem rateItem = null;
+            if(cursor!=null && cursor.moveToFirst()){
+                rateItem = new LogItem();
+                rateItem.setId(cursor.getInt(cursor.getColumnIndex("ID")));
+                rateItem.setLogs(cursor.getString(cursor.getColumnIndex("LOGS")));
+                rateItem.setTime(cursor.getString(cursor.getColumnIndex("TIME")));
+                cursor.close();
+            }
+            db.close();
+            return rateItem;
+        }
+    }
+
--- app/src/main/java/com/example/asus/moodlog/LogaddActivity.java
+++ app/src/main/java/com/example/asus/moodlog/LogaddActivity.java
@@ -2,12 +2,50 @@ package com.example.asus.moodlog;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class LogaddActivity extends AppCompatActivity {
+    private final String TAG = "LogaddActivity";
+    String writeDate;
+    EditText logadd;
+    Button save;
+    TextView dateText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logadd);
+        writeDate = (new SimpleDateFormat("yyyy-MM-dd")).format(new Date());
+        Log.i(TAG,"writeDate:" + writeDate);
+        save = (Button)findViewById(R.id.btnsave);
+        dateText=(TextView)findViewById(R.id.datetext);
+        dateText.setText(writeDate);
+    }
+    public void save(View btn){
+        Log.i(TAG, "onClick: ");
+        logadd = (EditText)findViewById(R.id.add_log);
+        String logstr = logadd.getText().toString();
+        if(logstr.length()>0){
+           //保存到数据库中
+            LogItem logItem= new LogItem();
+            logItem.setLogs(logstr);
+            logItem.setTime(writeDate);
+            DBManager dbManager = new DBManager(LogaddActivity.this);
+            dbManager.add(logItem);
+            Toast.makeText(this, "保存成功", Toast.LENGTH_SHORT).show();
+        }else{
+            //用户没有输入内容
+            Toast.makeText(this, "你还没有记录心情哦", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
     }
 }
