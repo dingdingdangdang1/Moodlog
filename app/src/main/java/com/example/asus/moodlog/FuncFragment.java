0e472ef34e49d1e2b46afae99c040695b0b9cb86
asus <2034233226@qq.com>
2019-06-23 15:00:03 +0800

    soupfinished

--- app/src/main/java/com/example/asus/moodlog/FuncFragment.java
+++ app/src/main/java/com/example/asus/moodlog/FuncFragment.java
@@ -1,11 +1,13 @@
 package com.example.asus.moodlog;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -23,7 +25,38 @@ public class FuncFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_func, container, false);
+        View view=inflater.inflate(R.layout.fragment_func, container, false);
+        Button btne=(Button)view.findViewById(R.id.btn_e);
+        Button btngrowth=(Button)view.findViewById(R.id.btn_growth);
+        Button btnlife=(Button)view.findViewById(R.id.btn_life);
+        btne.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //在这里使用getActivity
+                Intent intent = new Intent(getActivity(),EncourageActivity.class);
+                startActivity(intent);
+            }
+        });
+        btngrowth.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //在这里使用getActivity
+                Intent intent = new Intent(getActivity(),GrowthActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnlife.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //在这里使用getActivity
+                Intent intent = new Intent(getActivity(),LifeActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 
 }
---
+++ app/src/main/java/com/example/asus/moodlog/GrowthActivity.java
@@ -0,0 +1,104 @@
+package com.example.asus.moodlog;
+
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class GrowthActivity extends AppCompatActivity implements  Runnable {
+    private static final String TAG ="GrowthActivity" ;
+    private ArrayList<HashMap<String, String>> soupList; // 存放文字、图片信息
+    private SimpleAdapter listItemAdapter; // 适配器
+    ListView listView;
+    private int msgWhat = 3;
+    Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_growth);
+        TextView noda2=findViewById(R.id.nodata22);
+        listView=findViewById(R.id.soup_list2);
+        listView.setEmptyView(noda2);
+        Thread t = new Thread(this); // 创建新线程
+        t.start(); // 开启线程
+        handler = new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                if(msg.what == msgWhat){
+                    soupList = (ArrayList<HashMap<String, String>>) msg.obj;
+                    listItemAdapter = new SimpleAdapter(GrowthActivity.this, soupList, // listItems数据源
+                            R.layout.souplistf, // ListItem的XML布局实现
+                            new String[] { "ItemTitle", "ItemDetail" },
+                            new int[] { R.id.soup_detail, R.id.soup_rsc });
+                    listView.setAdapter(listItemAdapter);
+                    Log.i("handler","reset list...");
+                }
+                super.handleMessage(msg);
+            }
+
+        };
+
+
+
+    }
+    @Override
+    public void run() {
+        Log.i("thread","run.....");
+        boolean marker = false;
+        ArrayList<HashMap<String,String>> relist=new ArrayList<HashMap<String,String>>();
+
+        try {
+            Document doc = Jsoup.connect("https://www.juzimi.com/tags/%E6%88%90%E9%95%BF.header(\"User-Agent\",\"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36\")").get();
+            Elements soup = doc.select("div.views-field-phpcode");
+            for (Element element : soup) {//遍历数组
+                HashMap<String, String> map = new HashMap<String, String>();
+
+                String tdStr =element.select("a.xlistju").text();//查找element下的a标签鸡汤的内容
+                String author=element.select("a.views-field-field-oriwriter-value").text();
+                String book=element.select("a.active").text();
+                //查找出处的内容
+                String pStr ="——"+author+"《"+book+"》";
+                map.put("ItemTitle", tdStr);
+                map.put("ItemDetail", pStr);//将出处组合起来放入map中
+                relist.add(map);
+                Log.i("鸡汤",tdStr + "出处" + pStr);
+
+            }
+            marker = true;
+        } catch (MalformedURLException e) {
+            Log.e("www", e.toString());
+            e.printStackTrace();
+        } catch (IOException e) {
+            Log.e("www", e.toString());
+            e.printStackTrace();
+        }
+
+        Message msg = handler.obtainMessage();
+        msg.what = msgWhat;
+        if(marker){
+            msg.arg1 = 1;//标记操作成功与否
+        }else{
+            msg.arg1 = 0;
+        }
+
+        msg.obj = relist;
+        handler.sendMessage(msg);
+
+        Log.i("thread","sendMessage.....");
+    }
+}
